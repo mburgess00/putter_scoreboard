f98982bf47f2697740a2d853ead4ef9375f05351
mburgess00 <mburgess00@users.noreply.github.com>
2018-07-12 13:52:38 -0600

    initial rewrite with what was learned via testing
    timer start/stop and timer reset works, but nothing else.

--- putter_scoreboard.ino
+++ putter_scoreboard.ino
@@ -5,13 +5,10 @@
  5V -> 5V (Orange)
  Power Arduino with 12V and connect to Vin -> 12V (Red)
  GND -> GND (Black)
-
  There are two connectors on the Large Digit Driver. 'IN' is the input side that should be connected to
  your microcontroller (the Arduino). 'OUT' is the output side that should be connected to the 'IN' of addtional
  digits.
-
  Each display will use about 150mA with all segments and decimal point on.
-
 */
 
 #include <EEPROM.h>
@@ -23,29 +20,33 @@ byte segmentLatch = 5;
 byte segmentData = 7;
 
 //Remote receiver
-byte aPin = 10;
-byte bPin = 11;
-byte cPin = 12;
-byte dPin = 13;
+byte aPin = 13;
+byte bPin = 12;
+byte cPin = 11;
+byte dPin = 10;
 
 //Buzzer
 byte buzzer = 8;
 
 //Reset button
 byte resetButton = 9;
+
+const int timerLength = 10;
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 unsigned long previousMillis = 0;
 boolean timerRunning = false;
 boolean currentGame = false;
-int timer = 30;
+int timer = timerLength;
 int score = 0;
 int highscore = 0;
 int currentButton = 0; //none=0, A=1, B=2, C=3, D=4, reset=-1
 int previousButton =0;
-unsigned long presstime = 0;
+unsigned long pressTime = 0;
 
 const int eeAddress = 0;
+
+boolean noDecimals[4] = {false, false, false, false};
 
 void setup()
 {
@@ -68,9 +69,10 @@ void setup()
 
   //buzzer
   pinMode(buzzer, OUTPUT);
-  digitalWrite(buzzer, HIGH);
-  delay(250);
-  digitalWrite(buzzer, LOW);
+  //digitalWrite(buzzer, HIGH);
+  //delay(250);
+  //digitalWrite(buzzer, LOW);
+  tone(buzzer, 1200, 250);
 
   //reset
   pinMode(resetButton, INPUT_PULLUP);
@@ -81,91 +83,158 @@ void setup()
   {
     highscore = 0;
   }
+
+  sendString(' ', ' ', ' ', ' ', noDecimals);
 }
 
 
 void loop()
 {
-  //handle buttons
+  unsigned long currentMillis = millis();
+  
+  int timer1 = timer % 10;
+  int timer2 = timer / 10;
+  
+  int score1 = score % 10;
+  int score2 = score / 10;
+  
+  previousButton = currentButton;
   if (digitalRead(aPin) == HIGH)
-  
-  if (digitalRead(resetButton) == LOW)
   {
-    if (previousButton != -1)
-    {
-      presstime = millis();
-    }
-    else if (millis() - presstime > 2000)
-    {
-      highscore = 0;
-      //write 0 to eeprom 
-    }
-    currentButton = -1;
+    currentButton = 1;
+    Serial.println("got a");
   }
-  
-  if timerRunning
+  else if (digitalRead(bPin) == HIGH)
   {
-    if (digitalRead(aPin) == HIGH)
-    {
-      timerRunning = false;
-    }
-    if (millis() - previousMillis >= 1000)
-    {
-      //increment timer
-      timer -= 1;
-      previousMillis = millis();
-      updateDisplay(timer, score);
-      if (timer == 0)
-      {
-        //end of game logic
-        currentGame = false;
-      }
-    }
+    currentButton = 2;
+    Serial.println("got b");
+  }
+  else if (digitalRead(cPin) == HIGH)
+  {
+    currentButton = 3;
+    Serial.println("got c");
+  }
+  else if (digitalRead(dPin) == HIGH)
+  {
+    currentButton = 4;
+    Serial.println("got d");
+  }
+  else if (digitalRead(resetButton) == HIGH)
+  {
+    currentButton = -1;
+    Serial.println("got reset");
   }
   else
   {
-    if (digitalRead(aPin) == HIGH)
+    currentButton = 0;
+  }
+  
+  if (currentButton != previousButton)
+  {
+    if (previousButton == 0)
     {
-      timerRunning = true;
-      previousMillis = millis();
-      if currentGame
+      pressTime = currentMillis;
+    }
+    if (currentButton == 0)
+    {
+      //button has been lifted
+      Serial.print("Button: ");
+      Serial.print(previousButton);
+      Serial.print(" has been pressed for ");
+      Serial.println(currentMillis - pressTime);
+      
+      //here's where we do the magic of button presses, as the button has been released.
+      
+      switch (previousButton)
       {
-        
+        case 1: 
+          if (timerRunning)
+          {
+            timerRunning = false;
+          }
+          else if (timer > 0)
+          {
+            startTimer();
+          }
+          break;
+        case 2: 
+          if (!timerRunning)
+          {
+            timer = timerLength;
+            Serial.print("timer = ");
+            Serial.println(timer);
+            timer1 = timer % 10;
+            timer2 = timer / 10;
+            sendString(score2, score1, timer2, timer1, noDecimals);
+          }
+          break;
+        case 3: 
+          break;
+        case 4: 
+          break;
+        case -1: 
+          break;
       }
     }
   }
-}
+    
 
-void updateDisplay(int curtime, int curscore)
-{
-  showNumber(curscore);
-  showNumber(curtime);
-}
 
-//Takes a number and displays 2 numbers. Displays absolute value (no negatives)
-void showNumber(float value)
-{
-  int number = abs(value); //Remove negative signs and any decimals
 
-  //Serial.print("number: ");
-  //Serial.println(number);
-
-  for (byte x = 0 ; x < 2 ; x++)
+  if (timerRunning)
   {
-    int remainder = number % 10;
+    //Serial.println(currentMillis - previousMillis);
+    if (currentMillis - previousMillis >= 1000)
+    {
+      Serial.print("timer=");
+      Serial.println(timer);
+      timer--;
+      timer1 = timer % 10;
+      timer2 = timer / 10;
+      sendString(score2, score1, timer2, timer1, noDecimals);
+      if (timer == 0)
+      {
+        tone(buzzer, 1000, 1000);
+        timerRunning = 0;
+      }
+      previousMillis = currentMillis;
 
-    postNumber(remainder, false);
-
-    number /= 10;
+    }
   }
+  
+}
 
-  //Latch the current segment data
+void startTimer()
+{
+  int timer1 = timer % 10;
+  int timer2 = timer / 10;
+  sendString(' ', 3, timer2, timer1, noDecimals);
+  tone(buzzer, 1000, 250);
+  delay(750);
+  sendString(' ', 2, timer2, timer1, noDecimals);
+  tone(buzzer, 1000, 250);
+  delay(750);
+  sendString(' ', 1, timer2, timer1, noDecimals);
+  tone(buzzer, 1000, 250);
+  delay(750);
+  sendString('g', 'o', timer2, timer1, noDecimals);
+  tone(buzzer, 1200, 1000);
+  timerRunning = true;
+}
+
+
+void sendString(byte a, byte b, byte c, byte d, boolean decimals[4])
+{
+  postChar(d, decimals[3]);
+  postChar(c, decimals[2]);
+  postChar(b, decimals[1]);
+  postChar(a, decimals[0]);
   digitalWrite(segmentLatch, LOW);
-  digitalWrite(segmentLatch, HIGH); //Register moves storage register on the rising edge of RCK
+  digitalWrite(segmentLatch, HIGH);  
 }
 
 //Given a number, or '-', shifts it out to the display
-void postNumber(byte number, boolean decimal)
+void postChar(byte digit, boolean decimal)
 {
   //    -  A
   //   / / F/B
@@ -182,26 +251,42 @@ void postNumber(byte number, boolean decimal)
 #define g  1<<2
 #define dp 1<<7
 
-  byte segments;
+  //Serial.print("sending the following character: ");
+  //Serial.println(digit);
 
-  switch (number)
+  byte segments;
+  Serial.print("digit: ");
+  //Serial.println(digit);
+
+  switch (digit)
   {
-    case 1: segments = b | c; break;
-    case 2: segments = a | b | d | e | g; break;
-    case 3: segments = a | b | c | d | g; break;
-    case 4: segments = f | g | b | c; break;
-    case 5: segments = a | f | g | c | d; break;
-    case 6: segments = a | f | g | e | c | d; break;
-    case 7: segments = a | b | c; break;
-    case 8: segments = a | b | c | d | e | f | g; break;
-    case 9: segments = a | b | c | d | f | g; break;
-    case 0: segments = a | b | c | d | e | f; break;
-    case ' ': segments = 0; break;
-    case 'c': segments = g | e | d; break;
-    case '-': segments = g; break;
+    case 1: segments = b | c; Serial.println("1"); break;
+    case 2: segments = a | b | d | e | g; Serial.println("2"); break;
+    case 3: segments = a | b | c | d | g; Serial.println("3"); break;
+    case 4: segments = f | g | b | c; Serial.println("4"); break;
+    case 5: segments = a | f | g | c | d; Serial.println("5"); break;
+    case 6: segments = a | f | g | e | c | d; Serial.println("6"); break;
+    case 7: segments = a | b | c; Serial.println("7"); break;
+    case 8: segments = a | b | c | d | e | f | g; Serial.println("8"); break;
+    case 9: segments = a | b | c | d | f | g; Serial.println("9"); break;
+    case 0: segments = a | b | c | d | e | f; Serial.println("0"); break;
+    case ' ': segments = 0; Serial.println(" "); break;
+    case 'a': segments = a | b | c | e | f | g; Serial.println("a"); break;
+    case 'b': segments = c | d | e | f | g; Serial.println("b"); break;
+    case 'c': segments = g | e | d; Serial.println("c"); break;
+    case 'd': segments = b | c | d | e | g; Serial.println("d"); break;
+    case 'e': segments = a | d | e | f | g; Serial.println("e"); break;
+    case 'f': segments = a | e | f | g; Serial.println("f"); break;
+    case 'g': segments = a | b | c | d | f | g; Serial.println("g"); break;
+    case 'h': segments = c | e | f | g; Serial.println("h"); break;
+    case 'o': segments = a | b | f | g; Serial.println("o"); break;
+    case '-': segments = g; Serial.println("-"); break;
+    default: segments = 0;
   }
 
   if (decimal) segments |= dp;
+  //Serial.print("segments: ");
+  //Serial.println(segments);
 
   //Clock these bits out to the drivers
   for (byte x = 0 ; x < 8 ; x++)
@@ -210,5 +295,5 @@ void postNumber(byte number, boolean decimal)
     digitalWrite(segmentData, segments & 1 << (7 - x));
     digitalWrite(segmentClock, HIGH); //Data transfers to the register on the rising edge of SRCK
   }
-}
 
+}
